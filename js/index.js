e035ae6061db0011ce945fddd6e279bd4b3582f5
parkhun99 <2114959@naver.com>
2024-10-16 00:01:34 +0900

    하단배너 추가

--- js/index.js
+++ js/index.js
@@ -1,39 +1,39 @@
-$(document).ready(function() {
-    $(window).on('scroll', function() {
-        var scrollTop = $(window).scrollTop();
+// $(document).ready(function() {
+//     $(window).on('scroll', function() {
+//         var scrollTop = $(window).scrollTop();
   
-        if (scrollTop >= 500) {
-          $('.up_btn').stop().fadeTo(200, 1);
-        } else {
-          $('.up_btn').stop().fadeTo(200, 0);
-        }
+//         if (scrollTop >= 500) {
+//           $('.up_btn').stop().fadeTo(200, 1);
+//         } else {
+//           $('.up_btn').stop().fadeTo(200, 0);
+//         }
 
-        if(scrollTop > 0) {
-            $('header').css({background: '#fff'});
-            $('.top_items').css({color: '#000'});
-        } else if (scrollTop <= 0) {
-            $('header').css({background: 'transparent'});
-            $('.top_items').css({color: '#fff'});
-        }
+//         if(scrollTop > 0) {
+//             $('header').css({background: '#fff'});
+//             $('.top_items').css({color: '#000'});
+//         } else if (scrollTop <= 0) {
+//             $('header').css({background: 'transparent'});
+//             $('.top_items').css({color: '#fff'});
+//         }
 
-        if(scrollTop >= 350) {
-            setTimeout(() => {
-                $('.coffee_item').addClass('moveBig');
-            }, 500);
-            $('.text').addClass('moveUp3');
-        }
+//         if(scrollTop >= 350) {
+//             setTimeout(() => {
+//                 $('.coffee_item').addClass('moveBig');
+//             }, 500);
+//             $('.text').addClass('moveUp3');
+//         }
 
-        if(scrollTop >= 800) {
-            $('.third_section').addClass('moveLeft');
-        }
-    });
+//         if(scrollTop >= 800) {
+//             $('.third_section').addClass('moveLeft');
+//         }
+//     });
 
-    $('.up_btn').on('click touchstart', function(event) {
-        event.preventDefault(); event.stopPropagation();
+//     $('.up_btn').on('click touchstart', function(event) {
+//         event.preventDefault(); event.stopPropagation();
    
-        $('html, body').animate({ scrollTop: 0 }, 500);
-    });
-});
+//         $('html, body').animate({ scrollTop: 0 }, 500);
+//     });
+// });
 
 
 
